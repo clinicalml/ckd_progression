5a470704326e79a34356c9f96d019376fef67cc3
Jake Marcus <jmarcus@cs.nyu.edu>
2016-06-29 19:15:14 -0400

    crossvalidation + random sampling of hyperparams

--- nn.py
+++ nn.py
@@ -2,15 +2,48 @@ import tables
 import numpy as np
 import theano
 import theano.tensor as T
-#from theano.tensor.nnet.conv import conv2d
 from theano.tensor.signal.downsample import max_pool_2d
 import pdb
 import lasagne
 import sklearn.metrics
-from theano.compile.nanguardmode import NanGuardMode
+import yaml
+import random
 
-random_seed = 345
-srng = theano.tensor.shared_randomstreams.RandomStreams(seed=random_seed)
+def evaluate(results_fname, X_train, Y_train, X_validation, Y_validation, X_test, Y_test):
+
+	random_seed = 345
+	n_epochs = 32
+
+	best_valid_auc = -np.inf
+	best_model = None
+
+	np.random.seed(random_seed)
+
+	for i in range(100):
+		print i
+
+		n_hidden = np.random.randint(10, 200)
+		n_filters = np.random.randint(4, 32)
+		k_horiz = np.random.randint(2, 10)
+		dropout = np.random.uniform(0, 0.75)
+		init_learning_rate = np.random.uniform(0.001, 2)
+		rho = np.random.uniform(0.5, 0.95)	
+
+		model = NeuralNet(n_hidden, n_filters, k_horiz, dropout, init_learning_rate, rho, random_seed)
+		model.train_and_validate(n_epochs, X_train, Y_train, X_validation, Y_validation)
+		if model.best_valid_auc > best_valid_auc:
+			best_valid_auc = model.best_valid_auc
+			best_model = model
+
+	test_auc = best_model.calc_auc(X_test, Y_test)
+	
+	results = {}
+	results['valid_auc'] = float(best_model.best_valid_auc)
+	results['test_auc'] = float(test_auc)	
+	results['config'] = best_model.config
+	
+	with open(results_fname, 'w') as fout:
+		yaml.dump(results, fout)
 
 def get_data(in_fname):
 
@@ -25,96 +58,117 @@ def get_data(in_fname):
 
 	return X_train, Y_train, X_validation, Y_validation, X_test, Y_test
 
-def add_dropout(l, dropout, deterministic):
-	if dropout > 0:
-		if deterministic:
-			l = dropout*l
-		else:
-			l = T.switch(srng.binomial(size=l.shape, p=(1. - dropout)), l, 0)
-	return l
+class NeuralNet():
 
-def get_log_prob(X, w1, w2, b2, w3, b3, pool_horiz, n_conv, dropout, deterministic):
+	def __init__(self, n_hidden, n_filters, k_horiz, dropout, init_learning_rate, rho, random_seed):
 
-	l1 = T.nnet.relu(T.nnet.conv2d(X, w1, border_mode='valid', subsample=(1, 1)))
-	l2 = max_pool_2d(l1, ds=(1, pool_horiz), st=(1, 1), ignore_border=True)
-	l3 = l2.reshape((X.shape[0], n_conv))	
-	l3 = add_dropout(l3, dropout, deterministic)
-	l4 = T.nnet.relu(T.dot(l3, w2) + b2)
-	l4 = add_dropout(l4, dropout, deterministic)
-	l5 = T.dot(l4, w3) + b3
-	log_prob = T.nnet.logsoftmax(l5)
+		n_classes = 2
+		n_features = 15
+		n_time = 12
+		pool_horiz = 3
 
-	return log_prob 
+		self.config = {}
+		self.config['n_hidden'] = n_hidden	
+		self.config['n_filters'] = n_filters
+		self.config['k_horiz'] = k_horiz
+		self.config['dropout'] = dropout
+		self.config['init_learning_rate'] = init_learning_rate
+		self.config['rho'] = rho
+		self.config['random_seed'] = random_seed
 
-def build_model():
+		self.random_seed = random_seed
+		self.rng = random.Random(x=self.random_seed)
+		self.srng = theano.tensor.shared_randomstreams.RandomStreams(seed=random_seed)
 
-	X = T.tensor4()
-	Y = T.itensor4()
+		X = T.tensor4()
+		Y = T.itensor4()
+		
+		tdim1 = 1 + n_time - k_horiz
+		tdim2 = 1 + tdim1 - pool_horiz
+		n_conv = int(n_filters*n_features*tdim2)
 
-	n_hidden = 100
-	n_classes = 2
-	n_filters = 8
-	n_features = 15
-	n_time = 12
-	k_horiz = 8
-	pool_horiz = 3
-	dropout = 0
-	init_learning_rate = 0.01
+		w1 = theano.shared(value=self.init_weights((n_filters, 1, 1, k_horiz)))
+		w2 = theano.shared(value=self.init_weights((n_conv, n_hidden)))
+		b2 = theano.shared(value=np.zeros(n_hidden, dtype=theano.config.floatX))
+		w3 = theano.shared(value=self.init_weights((n_hidden, n_classes)))
+		b3 = theano.shared(value=np.zeros(n_classes, dtype=theano.config.floatX))
+
+		log_prob = self.get_log_prob(X, w1, w2, b2, w3, b3, pool_horiz, n_conv, dropout, deterministic=False)
+		test_log_prob = self.get_log_prob(X, w1, w2, b2, w3, b3, pool_horiz, n_conv, dropout, deterministic=True)
+
+		self.params = [w1, w2, b2, w3, b3]
+
+		y = Y[:,0,0,0]
+		prediction = T.argmax(log_prob, axis=1)
+		train_loss = -T.mean(log_prob[T.arange(X.shape[0]), y])
+
+		updates = lasagne.updates.adadelta(train_loss, self.params, learning_rate=init_learning_rate, rho=rho, epsilon=1e-06) 
+
+		self.train_fn = theano.function(inputs=[X, Y], outputs=train_loss, updates=updates, allow_input_downcast=True)
+		self.predict_fn = theano.function(inputs=[X], outputs=test_log_prob, allow_input_downcast=True) 
+
+	def init_weights(self, shape):
+		n = np.prod(shape)
+		x = np.array([self.rng.gauss(0, 1) for i in range(n)]) * 0.01
+		return np.asarray(x.reshape(shape), dtype=theano.config.floatX)
+
+	def add_dropout(self, l, dropout, deterministic):
+		if dropout > 0:
+			if deterministic:
+				l = dropout*l
+			else:
+				l = T.switch(self.srng.binomial(size=l.shape, p=(1. - dropout)), l, 0)
+		return l
+
+	def get_log_prob(self, X, w1, w2, b2, w3, b3, pool_horiz, n_conv, dropout, deterministic):
+
+		l1 = T.nnet.relu(T.nnet.conv2d(X, w1, border_mode='valid', subsample=(1, 1)))
+		l2 = max_pool_2d(l1, ds=(1, pool_horiz), st=(1, 1), ignore_border=True)
+		l3 = l2.reshape((X.shape[0], n_conv))	
+		l3 = self.add_dropout(l3, dropout, deterministic)
+		l4 = T.nnet.relu(T.dot(l3, w2) + b2)
+		l4 = self.add_dropout(l4, dropout, deterministic)
+		l5 = T.dot(l4, w3) + b3
+		log_prob = T.nnet.logsoftmax(l5)
+
+		return log_prob 
+
+	def calc_auc(self, X, Y):
+		proba = self.predict_fn(X)[:,1]
+		fpr, tpr, _ = sklearn.metrics.roc_curve(Y[:,0,0,0], proba)
+		auc = sklearn.metrics.auc(fpr, tpr)	
+		return auc
+
+	def load_params(self, new_params):
+		for p in range(len(self.params)):
+			self.params[p].set_value(new_params[p])
 	
-	tdim1 = 1 + n_time - k_horiz
-	tdim2 = 1 + tdim1 - pool_horiz
-	n_conv = int(n_filters*n_features*tdim2)
+	def train_and_validate(self, n_epochs, X_train, Y_train, X_validation, Y_validation):
 
-	w1 = theano.shared(value=np.asarray(np.random.randn(n_filters, 1, 1, k_horiz) * np.sqrt(1./float(n_filters*k_horiz)), dtype=theano.config.floatX))
-	w2 = theano.shared(value=np.asarray(np.random.randn(n_conv, n_hidden) * 0.01, dtype=theano.config.floatX))
-	b2 = theano.shared(value=np.zeros(n_hidden, dtype=theano.config.floatX))
-	w3 = theano.shared(value=np.asarray(np.random.randn(n_hidden, n_classes) * 0.01, dtype=theano.config.floatX))
-	b3 = theano.shared(value=np.zeros(n_classes, dtype=theano.config.floatX))
+		self.rng = random.Random(x=self.random_seed)
+	
+		mini_batch_size = 256
+		n_examples = X_train.shape[0]
 
-	log_prob = get_log_prob(X, w1, w2, b2, w3, b3, pool_horiz, n_conv, dropout, deterministic=False)
-	test_log_prob = get_log_prob(X, w1, w2, b2, w3, b3, pool_horiz, n_conv, dropout, deterministic=True)
+		self.best_valid_auc = -np.inf
+		self.best_valid_epoch = None
+		self.best_params = None
+		for epoch in range(n_epochs):
+			indices = np.arange(n_examples)
+			self.rng.shuffle(indices)
+			X = X_train[indices]
+			Y = Y_train[indices]
+			print indices[0:10]
 
-	params = [w1, w2, b2, w3, b3]
+			for start, stop in zip(range(0, n_examples, mini_batch_size), range(mini_batch_size, n_examples, mini_batch_size)):
+				train_loss = self.train_fn(X[start:stop], Y[start:stop])
 
-	#return theano.function(inputs=[X], outputs=l2, allow_input_downcast=True)
+			train_auc = self.calc_auc(X_train, Y_train)
+			valid_auc = self.calc_auc(X_validation, Y_validation)
+	
+			if valid_auc > self.best_valid_auc:
+				self.best_valid_auc = valid_auc
+				self.best_valid_epoch = epoch
+				self.best_params = [param.get_value() for param in self.params]
 
-	y = Y[:,0,0,0]
-	prediction = T.argmax(log_prob, axis=1)
-	train_loss = -T.mean(log_prob[T.arange(X.shape[0]), y])
-
-	updates = lasagne.updates.adadelta(train_loss, params, learning_rate=init_learning_rate, rho=0.95, epsilon=1e-06) 
-	#grads = T.grad(train_loss, params)
-	#updates = [(param, param - init_learning_rate*grad) for param, grad in zip(params, grads)]
-
-	train_fn = theano.function(inputs=[X, Y], outputs=train_loss, updates=updates, allow_input_downcast=True) # mode=NanGuardMode(nan_is_error=True, inf_is_error=True, big_is_error=True))
-	predict_fn = theano.function(inputs=[X], outputs=test_log_prob, allow_input_downcast=True) 
-
-	return train_fn, predict_fn
-
-def calc_auc(X, Y, predict_fn):
-	proba = predict_fn(X)[:,1]
-	fpr, tpr, _ = sklearn.metrics.roc_curve(Y[:,0,0,0], proba)
-	auc = sklearn.metrics.auc(fpr, tpr)	
-	return auc
-
-def evaluate(X_train, Y_train, X_validation, Y_validation, X_test, Y_test):
-
-	train_fn, predict_fn = build_model()
-
-	n_epochs = 5
-	mini_batch_size = 256
-	n_examples = X_train.shape[0]
-
-	for epoch in range(n_epochs):
-		print epoch
-		for start, stop in zip(range(0, n_examples, mini_batch_size), range(mini_batch_size, n_examples, mini_batch_size)):
-			train_loss = train_fn(X_train[start:stop], Y_train[start:stop])
-
-		train_auc = calc_auc(X_train, Y_train, predict_fn)
-		valid_auc = calc_auc(X_validation, Y_validation, predict_fn)
-		print train_auc
-		print valid_auc		
-
-	test_auc = calc_auc(X_test, Y_test, predict_fn)
-	print
-	print test_auc
+		self.load_params(self.best_params)	
